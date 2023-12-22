84f57753afe1eddedc5ee760a3252cdb24eb0485
AlbieriGarcia <albieri404@gmail.com>
2023-12-21 21:39:36 -0400

    Fix: absolute routes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,12 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: theme =>({
-        'lego-block': 'url(../img/bq.png)',
-        'lego-block-down': 'url(../img/bq-upside-down.png)',
-        'lego-block-down-pink': 'url(../img/bq-upside-down-pink.png)',
-        'lego-block-down-blue': 'url(../img/bq-upside-down-blue.png)',
-        'red-bg': 'url(../img/red-background.webp)',
-        'character-1': 'url(../img/character-1.png)',
-        'free-skin': 'url(../img/free-skin.webp)'
+        'lego-block': 'url(/img/bq.png)',
+        'lego-block-down': 'url(/img/bq-upside-down.png)',
+        'lego-block-down-pink': 'url(/img/bq-upside-down-pink.png)',
+        'lego-block-down-blue': 'url(/img/bq-upside-down-blue.png)',
+        'red-bg': 'url(/img/red-background.webp)',
+        'character-1': 'url(/img/character-1.png)',
       }),
       backgroundColor: theme => ({
         'lego-red': 'rgb(227, 0, 10)'

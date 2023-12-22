d2898465b5957444c323a24f42a15c536a12db7c
AlbieriGarcia <albieri404@gmail.com>
2023-12-21 20:20:10 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      backgroundImage: theme =>({
+        'lego-block': 'url(../img/bq.png)',
+        'lego-block-down': 'url(../img/bq-upside-down.png)',
+        'lego-block-down-pink': 'url(../img/bq-upside-down-pink.png)',
+        'lego-block-down-blue': 'url(../img/bq-upside-down-blue.png)',
+        'red-bg': 'url(../img/red-background.webp)',
+        'character-1': 'url(../img/character-1.png)',
+        'free-skin': 'url(../img/free-skin.webp)'
+      }),
+      backgroundColor: theme => ({
+        'lego-red': 'rgb(227, 0, 10)'
+      })
+    },
+  },
+  plugins: [],
+}
+
